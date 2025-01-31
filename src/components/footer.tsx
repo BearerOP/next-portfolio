911aaed010906e2bf427f40fe771eb77ed2900ab
BearerOP <ankity1892003@gmail.com>
2025-01-31 18:25:13 +0530

    Update resume link in footer component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -21,7 +21,7 @@ const socialLinks = [
         label: "Mail",
     },
     {
-        href: "https://dub.sh/NdeEWE2",
+        href: "https://dub.sh/resume189",
         label: "Resume",
     },
 ];
