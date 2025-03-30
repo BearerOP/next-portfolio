0dd37bbeec7973357b88ac2b37859124e26ed0c0
BearerOP <ankity1892003@gmail.com>
2025-03-30 16:51:24 +0530

    Update metadata for portfolio with enhanced Open Graph and Twitter card details

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,17 @@ import DotPattern from "@/components/magicui/dot-pattern";
 export default function Home() {
   return (
     <>
-          <main className="max-w-7xl mx-auto flex flex-col gap-4 px-4 min-h-[92vh]">
-    <DotPattern
-       width={5}
-       height={5}
-       cx={1}
-       cy={1}
-       cr={1}
-       className={cn(
-         "[mask-image:linear-gradient(to_bottom_right,white,transparent,transparent)] ",
-       )}
-      />
+      <main className="max-w-7xl mx-auto flex flex-col gap-4 px-4 min-h-[92vh]">
+        <DotPattern
+          width={5}
+          height={5}
+          cx={1}
+          cy={1}
+          cr={1}
+          className={cn(
+            "[mask-image:linear-gradient(to_bottom_right,white,transparent,transparent)] "
+          )}
+        />
         <nav className="flex justify-between items-center p-4 flex-wrap gap-2">
           <div className="flex items-center gap-2">
             <Logo />
@@ -29,7 +29,7 @@ export default function Home() {
 
         {/* Add responsive classes to the carousel */}
         <ProjectCarousel />
-        <Toaster/>
+        <Toaster />
       </main>
     </>
   );
