67373e3e073d8799167aa0fafd64c23867caccee
BearerOP <ankity1892003@gmail.com>
2025-03-30 16:54:21 +0530

    Update portfolio metadata for improved clarity and consistency

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -2,43 +2,20 @@
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "Ankit's Portfolio",
-  description: "A Personal portfolio of a Full Stack Developer.",
-  openGraph: {
-    url: "https://bearerop.tech",
-    type: "website",
-    title: "Ankit's Portfolio",
-    description: "A Personal portfolio of a Full Stack Developer.",
-    images: [
-      {
-        url: "https://firebasestorage.googleapis.com/v0/b/theslugproject.appspot.com/o/portfolio%2FIMG_4874-3.jpg?alt=media&token=35d78679-3f58-4cee-909e-008d136f0280",
-        width: 1200,
-        height: 630,
-        alt: "Ankit's Portfolio",
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    site: "@yourTwitterHandle", // Replace with your Twitter handle if available
-    title: "Ankit's Portfolio",
-    description: "A Personal portfolio of a Full Stack Developer.",
-    images: [
-      "https://firebasestorage.googleapis.com/v0/b/theslugproject.appspot.com/o/portfolio%2FIMG_4874-3.jpg?alt=media&token=35d78679-3f58-4cee-909e-008d136f0280",
-    ],
-  },
+  title: 'Portfolio',
+  description: 'My Personal portfolio.',
   icons: [
     {
-      rel: "icon",
-      type: "image/x-icon",
-      url: "/favicon.ico",
-      media: "(prefers-color-scheme: light)",
+      rel: 'icon',
+      type: 'image/x-icon',
+      url: '/favicon.ico',
+      media: '(prefers-color-scheme: light)',
     },
     {
-      rel: "icon",
-      type: "image/png",
-      url: "/favicon.ico",
-      media: "(prefers-color-scheme: dark)",
+      rel: 'icon',
+      type: 'image/png',
+      url: '/favicon.ico',
+      media: '(prefers-color-scheme: dark)',
     },
   ],
 };
